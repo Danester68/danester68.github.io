1ef63bab3a80a38b7a649737de7a95b6ae9a1092
Danester68 <kjdyson68@gmail.com>
2025-12-30 15:56:41 -0500

    Add id head to head elements, update loadContent.js, add credits, and add head-references

--- js/loadContent.js
+++ js/loadContent.js
@@ -2,6 +2,16 @@ window.onload = function() {
     fetch("/home-page")
         .then(response => response.text())
         .then(homePageFile => {
-            document.getElementById("menu").innerHTML = homePageFile
-        })
+            document.getElementById("menu").innerHTML = homePageFile;
+        });
+    fetch("/head-references")
+        .then(response => response.text())
+        .then(headReferencesFile => {
+            console.log("1");
+            const lines = headReferencesFile.split("*");
+            for (i = 0; i < lines.length; i++) {
+                console.log("2 (" + i + ")");
+                document.getElementById("head").appendChild(document.createTextNode(lines[i]));
+            }
+        });
 }

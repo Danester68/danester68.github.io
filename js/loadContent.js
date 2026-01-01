414f359bf2e6499c909f62f02324bbb72a187b71
Danester68 <kjdyson68@gmail.com>
2025-12-31 20:51:21 -0500

    Revert previous changes

--- js/loadContent.js
+++ js/loadContent.js
@@ -4,14 +4,4 @@ window.onload = function() {
         .then(homePageFile => {
             document.getElementById("menu").innerHTML = homePageFile;
         });
-    fetch("/head-references")
-        .then(response => response.text())
-        .then(headReferencesFile => {
-            console.log("1");
-            const lines = headReferencesFile.split("*");
-            for (i = 0; i < lines.length; i++) {
-                console.log("2 (" + i + ")");
-                document.getElementById("head").appendChild(document.createTextNode(lines[i]));
-            }
-        });
 }
